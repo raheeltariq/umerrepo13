ef982cae5bb0945e5dfabcc0ee3409865bb0cb75
raheeltariq <73988046+raheeltariq@users.noreply.github.com>
2022-09-04 00:37:51 +0500

    update 66

---
+++ argument_script000.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# created and updated on 19th july
+# run this script with a few arguments
+
+echo The first argument is $1
+
+echo The second argument is $2
+
+echo The third argument is $3
